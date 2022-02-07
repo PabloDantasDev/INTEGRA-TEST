2bdf8b3bd5b74b3ed20cbe3702a775adbafa32ea
PabloDantasDev <pabloj72017@gmail.com>
2022-02-06 23:49:09 -0300

    Adicionar API CPF

--- js/script.js
+++ js/script.js
@@ -95,3 +95,55 @@ document.getElementById('cepbtn').addEventListener('click',pesquisaCep);
 
 
 
+const pesquisaCPF = async ()=> {
+ 
+
+    const preencheformulario =(msg)=>{
+  
+      document.getElementById('nome').value=msg.msg.nome
+      document.getElementById('sexo').value=msg.msg.sexo
+      document.getElementById('nascimento').value=msg.msg.nascimento
+      document.getElementById('endereco').value=msg.msg.enderecos[0].endereco
+      document.getElementById('ccidade').value=msg.msg.enderecos[0].cidade
+      document.getElementById('bbairro').value=msg.msg.enderecos[0].bairro
+      document.getElementById('cccep').value=msg.msg.enderecos[0].cep
+      document.getElementById('eestado').value=msg.msg.enderecos[0].estado
+      document.getElementById('nnumero').value=msg.msg.enderecos[0].numero
+    }
+    var settings = {
+      url: "http://searchlock.me/api/v1/SEARCHLOCK/cpf/09665678469",
+      method: "GET",
+       mode:'cors',
+       crossDomain: true,
+      timeout: 0,
+      headers: {
+        accept: "application/json",
+        "Authorization": "Bearer APP_3b69d078903e239ee5679e34cb6135a1"
+      },
+    };
+  
+   
+  
+     $.ajax(settings)({
+          url: `https://searchlock.me/api/v1/SEARCHLOCK/cpf/09665678469`,settings,//
+          
+  
+          beforeSend: function (xhr) {
+            
+            xhr.setRequestHeader('Authorization',
+             'Bearer APP_3b69d078903e239ee5679e34cb6135a1', );     
+        }, })
+        
+        .done(function(msg) {
+          var msg =JSON.parse(msg)
+          console.log(msg);
+          preencheformulario(msg);
+        })
+      }
+  
+         const preencheformulario = (msg)=>{
+  
+        
+      }
+  
+  document.getElementById('pcpf').addEventListener('click',pesquisaCPF);
